f3d5159ab4d73efec217af7a14d9c1ce64802f26
MohamedBelalx <hero.armna@gmail.com>
2019-10-26 19:45:09 +0200

    add pages for qr code

---
+++ content/Robotics.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/mdb.css">
+    <style>
+        div
+        {
+            padding-top:50px;
+            text-transform:capitalize
+        }
+    </style>
+</head>
+<body>
+<div class='container'>
+    <h1>Robotics WorkShop:</h1>
+    <p>Robotics committee aims to introduce embedded systems and interfacing with sensors and motors and different devices using Arduino. </p>
+    <h3>WorkShop Content :</h3>
+    <ul>
+        <li>Introduction to history of robotics, ICs</li>
+        <li>Difference between microcontroller and microprocessor</li>
+        <li>Arduino and its layout. </li>
+    </ul>
+</div>
+</body>
+</html>
